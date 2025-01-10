7c5b28b10ddcfebb7e4bfb02e1c042c0183ea903
siem van keulen <vankeulensiem@gmail.com>
2025-01-10 18:49:48 +0100

    goed.html

--- waaromisingedachtenietgratis.js
+++ waaromisingedachtenietgratis.js
@@ -1,11 +1,11 @@
-let vragen = ["je","il"];
-let antwoorden = ["ik","hij"];
+let vragen = ["je", "il"];
+let antwoorden = ["ik", "hij"];
 let antwoord_oud = antwoorden;
 let mode = 1
 let vraag = 0;
 let MEERKEUZE_ANDWOORD_G_of_F = false;
 
-function nieuwe_vraag(){
+function nieuwe_vraag() {
     if (vragen.length == 0) {
         // reload the page to clear evrything
         window.location.reload();
@@ -17,23 +17,30 @@ function vraag_UI() {
 }
 function antwoord_UI() {
     document.getElementById("antwoord").innerHTML = antwoorden[vraag];
-    if(mode == 2){
+    if (mode == 2) {
         console.log("MEERKEUZE")
     }
 }
 function AntwoordGoed() {
     vragen.splice(vraag, 1);
     antwoorden.splice(vraag, 1);
-    if(mode == 1){
-    goTo("gostudy.html");
-    return
+    if (mode == 1) {
+        goTo("gostudy.html");
+        return
     }
-    if(mode == 2){
-        goTo("MEERKEUZE.html");
+    if (mode == 2) {
+        goTo("goed.html");
         return
     }
 
 }
-function MEERKEUZE_ANDWOORD_UI(){
+function MEERKEUZE_ANDWOORD_UI() {
     document.getElementById("MEERKEUZE_ANDWOORD").innerHTML = document.getElementById("MEERKEUZE_ANDWOORD").innerHTML + vragen[vraag];
+}
+
+function AntwoordFout() {
+    if (mode == 2) {
+        goTo("MEERKEUZE_ANDWOORD.html");
+        return
+    }
 }

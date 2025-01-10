84500420da5d9c304318beb5853fc159dfc35b55
siem van keulen <vankeulensiem@gmail.com>
2025-01-10 18:31:04 +0100

    beta

--- waaromisingedachtenietgratis.js
+++ waaromisingedachtenietgratis.js
@@ -1,6 +1,9 @@
 let vragen = ["je","il"];
 let antwoorden = ["ik","hij"];
+let antwoord_oud = antwoorden;
+let mode = 1
 let vraag = 0;
+let MEERKEUZE_ANDWOORD_G_of_F = false;
 
 function nieuwe_vraag(){
     if (vragen.length == 0) {
@@ -14,10 +17,23 @@ function vraag_UI() {
 }
 function antwoord_UI() {
     document.getElementById("antwoord").innerHTML = antwoorden[vraag];
+    if(mode == 2){
+        console.log("MEERKEUZE")
+    }
 }
 function AntwoordGoed() {
     vragen.splice(vraag, 1);
     antwoorden.splice(vraag, 1);
+    if(mode == 1){
     goTo("gostudy.html");
+    return
+    }
+    if(mode == 2){
+        goTo("MEERKEUZE.html");
+        return
+    }
 
+}
+function MEERKEUZE_ANDWOORD_UI(){
+    document.getElementById("MEERKEUZE_ANDWOORD").innerHTML = document.getElementById("MEERKEUZE_ANDWOORD").innerHTML + vragen[vraag];
 }

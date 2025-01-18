a89571d988c5d7f80f13b7c5de60feb4aae2c15c
supersiem <vankeulensiem@gmail.com>
2025-01-18 11:30:41 +0100

    idc

--- waaromisingedachtenietgratis.js
+++ waaromisingedachtenietgratis.js
@@ -1,6 +1,8 @@
 let vragen = ["je", "il"];
 let antwoorden = ["ik", "hij"];
+const MEERKEUZE_KEUZES = ["MEERKEUZE_A", "MEERKEUZE_B", "MEERKEUZE_C", "MEERKEUZE_D"];
 let antwoord_oud = antwoorden;
+let vragen_oud = vragen;
 let mode = 1
 let vraag = 0;
 let MEERKEUZE_ANDWOORD_G_of_F = false;
@@ -14,12 +16,46 @@ function nieuwe_vraag() {
 }
 function vraag_UI() {
     document.getElementById("vraag").innerHTML = vragen[vraag];
+    if (mode == 2) {
+        let MEERKEUZE_KEUZES_temp = [...MEERKEUZE_KEUZES];
+        temp = Math.floor(Math.random() * 4);
+
+        document.getElementById(MEERKEUZE_KEUZES_temp[temp]).innerHTML = antwoorden[vraag];
+        document.getElementById(MEERKEUZE_KEUZES_temp[temp]).setAttribute('href', "javascript:eval('AntwoordGoed()')");
+        MEERKEUZE_KEUZES_temp.splice(temp, 1);
+        MEERKEUZE_KEUZES_temp.forEach(element => {
+            let jeff = document.getElementById(MEERKEUZE_KEUZES_temp[MEERKEUZE_KEUZES_temp.indexOf(element)])
+            let random = Math.floor(Math.random() * antwoorden.length)
+            if (vragen[random] == vraag) {
+                return null
+            }
+            jeff.innerHTML = antwoorden[random];
+            jeff.setAttribute('href', "javascript:eval('AntwoordFout()')");
+            MEERKEUZE_KEUZES_temp.splice(MEERKEUZE_KEUZES_temp.indexOf(element), 1);
+        });
+        let jeff = document.getElementById(MEERKEUZE_KEUZES_temp[0])
+        let random = Math.floor(Math.random() * antwoorden.length)
+        if (vragen[random] == vraag) {
+            let random = Math.floor(Math.random() * antwoorden.length)
+        }
+
+        if (vragen[random] == vraag) {
+            let random = Math.floor(Math.random() * antwoorden.length)
+        }
+
+        if (vragen[random] == vraag) {
+            let random = Math.floor(Math.random() * antwoorden.length)
+        }
+
+        if (vragen[random] == vraag) {
+            let random = Math.floor(Math.random() * antwoorden.length)
+        }
+        jeff.innerHTML = antwoorden[random];
+        jeff.setAttribute('href', "javascript:eval('AntwoordFout()')");
+    }
 }
 function antwoord_UI() {
     document.getElementById("antwoord").innerHTML = antwoorden[vraag];
-    if (mode == 2) {
-        console.log("MEERKEUZE")
-    }
 }
 function AntwoordGoed() {
     vragen.splice(vraag, 1);
@@ -35,7 +71,7 @@ function AntwoordGoed() {
 
 }
 function MEERKEUZE_ANDWOORD_UI() {
-    document.getElementById("MEERKEUZE_ANDWOORD").innerHTML = document.getElementById("MEERKEUZE_ANDWOORD").innerHTML + vragen[vraag];
+    document.getElementById("MEERKEUZE_ANDWOORD").innerHTML = document.getElementById("MEERKEUZE_ANDWOORD").innerHTML + antwoorden[vraag];
 }
 
 function AntwoordFout() {
@@ -43,4 +79,8 @@ function AntwoordFout() {
         goTo("MEERKEUZE_ANDWOORD.html");
         return
     }
+}
+function startup() {
+    antwoord_oud = antwoorden;
+    vragen_oud = vragen;
 }

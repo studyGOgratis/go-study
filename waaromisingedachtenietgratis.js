1687369b920bb07f234f61270ccc02dc1f0cf3bb
supersiem <vankeulensiem@gmail.com>
2025-01-21 22:35:15 +0100

    fix??

--- waaromisingedachtenietgratis.js
+++ waaromisingedachtenietgratis.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 let vragen = ["je","il"];
 let antwoorden = ["ik","hij"];
 let lasted_vraag = "sdigfysgyfegfygsuygyfghdmncsbyuuaihwuay8748wy468w7yhduybfkyzytfeufiuysudkbfaw736i8w7yhaiuw3hbqyik27yqo0o78ieygfoiwubhrfuiyr8ai7yewiouhs";
+=======
+let vragen = ["je", "il"];
+let antwoorden = ["ik", "hij"];
+const MEERKEUZE_KEUZES = ["MEERKEUZE_A", "MEERKEUZE_B", "MEERKEUZE_C", "MEERKEUZE_D"];
+let antwoord_oud = antwoorden;
+let vragen_oud = vragen;
+let mode = 1
+>>>>>>> parent of a530fab (Merge branch 'main' into beta)
 let vraag = 0;
 
 function nieuwe_vraag(){
@@ -9,11 +18,6 @@ function nieuwe_vraag(){
         window.location.reload();
     }
     vraag = Math.floor(Math.random() * vragen.length);
-    if (vraag == lasted_vraag && ! vragen.length == 1) {
-        nieuwe_vraag();
-        
-    }
-    lasted_vraag = vraag;
 }
 function vraag_UI() {
     document.getElementById("vraag").innerHTML = vragen[vraag];
@@ -27,3 +31,20 @@ function AntwoordGoed() {
     goTo("gostudy.html");
 
 }
+<<<<<<< HEAD
+=======
+function MEERKEUZE_ANDWOORD_UI() {
+    document.getElementById("MEERKEUZE_ANDWOORD").innerHTML = document.getElementById("MEERKEUZE_ANDWOORD").innerHTML + antwoorden[vraag];
+}
+
+function AntwoordFout() {
+    if (mode == 2) {
+        goTo("MEERKEUZE_ANDWOORD.html");
+        return
+    }
+}
+function startup() {
+    antwoord_oud = antwoorden;
+    vragen_oud = vragen;
+}
+>>>>>>> parent of a530fab (Merge branch 'main' into beta)
